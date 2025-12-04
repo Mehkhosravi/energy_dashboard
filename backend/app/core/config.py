9ec2254d7f2ba27faf1d8a24c09c04a5f417706b
Mehrnaz <mehrnazz.khosravi@gmail.com>
2025-12-04 18:54:27 +0100

    add __init__.py for every python folder plus adding data to config

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,11 +1,11 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DB_USER: str
-    DB_PASSWORD: str
+    DB_USER: str = "postgres"
+    DB_PASSWORD: str = "profghadri"
     DB_HOST: str = "localhost"
     DB_PORT: int = 5432
-    DB_NAME: str
+    DB_NAME: str = "energy_data"
 
     class Config:
         env_file = ".env"
