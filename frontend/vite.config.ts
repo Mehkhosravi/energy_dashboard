405538c541f74a8ee7f14218a798713764b1988d
Keyvan Sharokhi <keyvan.shaarokhi@gmail.com>
2026-02-15 23:31:35 +0000

    fix: Update base path for GitHub Pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
   return {
-    base: mode === "production" ? "/EnergyPlatform/" : "/",
+    base: mode === "production" ? "/energy_dashboard/" : "/",
     plugins: [react()],
     build: {
       outDir: "dist", // changed from ../static/dist to standard dist for GH Pages
